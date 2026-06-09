a303b63d095f206063c7f0f1a7d29df5cf39e899
liam <l2258054087@126.com>
2026-06-09 18:44:53 +0800

    Create base_din.py

---
+++ base_din.py
@@ -0,0 +1,14 @@
+```python
+import torch
+import os
+import torch
+import numpy as np
+import pandas as pd
+import argparse # 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import roc_auc_score, f1_score
+
+```
+
+
